faf16f77d4ee92f6fc5a8c46535e7d66f58d665e
Patrick San Juan <Patrick.SanJuan@moodys.com>
2018-12-13 12:34:51 -0800

    Comments

--- app.js
+++ app.js
@@ -44,20 +44,20 @@ function dataParse(data) {
 
 function sortKeys(data) {
   let sorted = [];
+
   for (var obj in data) {
     let tmp = [];
     for (var key in data[obj]) {
-      // console.log(key);
-      tmp.push([key, data[obj][key]]);
+      tmp.push([key, data[obj][key]]); // Convert objects into an array of subarrays, containing key/value pairs
     }
-    tmp.sort((a, b) => (a[0] < b[0] ? -1 : 1));
+    tmp.sort((a, b) => (a[0] < b[0] ? -1 : 1)); // Sort key value pairs within sub-array
     sorted.push(tmp);
   }
-  sorted.sort((a, b) => (a[0][1] < b[0][1] ? -1 : 1));
+
+  sorted.sort((a, b) => (a[0][1] < b[0][1] ? -1 : 1)); // sort objects (which are now sub-arrays)
   return sorted;
 }
 
-// console.log(sortKeys(data));
-console.log(dataParse(data));
-console.log(dataParse(data2));
-console.log(dataParse(data2) === dataParse(data));
+console.log(dataParse(data)); // => d86c56673a72a2706baab514aace9e6f577fb848495b52c62ac20abd94a43e4d
+console.log(dataParse(data2)); // => d86c56673a72a2706baab514aace9e6f577fb848495b52c62ac20abd94a43e4d
+console.log(dataParse(data2) === dataParse(data)); // => True
